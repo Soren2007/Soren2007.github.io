5787755429aa672c564353812a9b103b5b2b72cd
Soren Shamloo <sorenshamloo.programmer@gmail.com>
2025-05-15 00:24:50 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// progress
+let skill_cards = document.querySelectorAll('.skill__card');
+skill_cards.forEach(element => {
+    element.addEventListener("mouseenter",() => {
+        let value = element.getAttribute("data-value"),
+            startValue = 0,
+            endValue = Number(value),
+            speed = 60
+        const progressText = element.querySelector(".progress__text__number"),
+            progressValue = element.querySelector(".progress__value"),
+            progress = setInterval(() => {
+            startValue++;
+            progressText.innerHTML = `${startValue}`;
+            progressValue.style.width = `${startValue}%`
+            if (startValue == endValue) {
+            clearInterval(progress);
+            }
+        }, speed);
+        element.addEventListener("mouseleave",() => {
+            clearInterval(progress);
+        })
+    })
+});
+// progress
